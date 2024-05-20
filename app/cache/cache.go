4f76eb7ebdc247e7089b6c9fc25ee90745c2c806
Ryan Adi Putra <ryannadiputraa@gmail.com>
2024-05-20 19:13:42 +0800

    chore(cache): compare remote tag

--- app/cache/cache.go
+++ app/cache/cache.go
@@ -16,7 +16,7 @@ type Cache struct {
 const (
 	userPermission = fs.FileMode(0700)
 	cacheFile      = "cache.json"
-	initTag        = "v0.0.0"
+	InitTag        = "v0.0.0"
 )
 
 func GetCache() (cache Cache, err error) {
@@ -32,7 +32,7 @@ func GetCache() (cache Cache, err error) {
 			return
 		}
 		// return init tag to continue proccess
-		return Cache{Tag: initTag}, nil
+		return Cache{Tag: InitTag}, nil
 	}
 	if err != nil {
 		return Cache{}, fmt.Errorf("fail to check cache status: %v", err)
@@ -42,7 +42,7 @@ func GetCache() (cache Cache, err error) {
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
 			// return init tag to continue proccess
-			return Cache{Tag: initTag}, nil
+			return Cache{Tag: InitTag}, nil
 		}
 		return
 	}
--- app/project/project.go
+++ app/project/project.go
@@ -1,21 +1,46 @@
 package project
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"io/fs"
+	"log"
 	"net/http"
 	"os"
 	"strings"
 
+	"github.com/ryanadiputraa/ggen/app/cache"
 	"github.com/ryanadiputraa/ggen/config"
+	"github.com/ryanadiputraa/ggen/pkg/github"
 )
 
 const (
 	userPermission = fs.FileMode(0700)
-	templateURL    = "https://raw.githubusercontent.com/ryanadiputraa/ggen/main"
 )
 
 func GenerateProjectTempalate(config *config.Config) (err error) {
+	isUseCache := false
+	c := cache.Cache{Tag: cache.InitTag}
+
+	fmt.Println("checking cache...")
+	c, err = cache.GetCache()
+	if err != nil {
+		log.Println("fail to check cache: ", err)
+	}
+
+	tag, err := checkTag()
+	if err != nil {
+		log.Println("fail to get latest tag: ", err)
+	} else {
+		isUseCache = c.Tag == tag
+	}
+
+	if !isUseCache {
+		fmt.Println("fetching template... (skipping cache")
+		cache.StoreCache(cache.Cache{Tag: tag})
+	}
+
 	if err = writeConfigFile(config); err != nil {
 		return
 	}
@@ -31,7 +56,7 @@ func GenerateProjectTempalate(config *config.Config) (err error) {
 	return writePkg(config)
 }
 
-// tmplPath is the file path from templateURL base path
+// tmplPath is the file path from github.TemplateURL base path
 func generateTemplateFile(config *config.Config, tmplPath, destPath string) (err error) {
 	content, err := fetchTemplate(tmplPath)
 	if err != nil {
@@ -48,7 +73,7 @@ func writeFile(config *config.Config, content, destPath string) (err error) {
 }
 
 func fetchTemplate(path string) (content string, err error) {
-	resp, err := http.Get(templateURL + path)
+	resp, err := http.Get(github.TemplateURL + path)
 	if err != nil {
 		return
 	}
@@ -61,4 +86,22 @@ func fetchTemplate(path string) (content string, err error) {
 
 	content = string(body)
 	return
+}
+
+func checkTag() (tag string, err error) {
+	resp, err := http.Get(github.TagURL)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	var tags []github.Tag
+	if err = json.NewDecoder(resp.Body).Decode(&tags); err != nil {
+		return
+	}
+	if len(tags) < 1 {
+		return cache.InitTag, nil
+	}
+
+	return tags[0].Name, nil
 }
