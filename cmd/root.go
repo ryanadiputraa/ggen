9be50194632b9f8b7e132e5ed120b72039e46323
Ryan Adi Putra <ryannadiputraa@gmail.com>
2024-05-29 22:29:25 +0800

    fix: cli version

--- cmd/root.go
+++ cmd/root.go
@@ -7,7 +7,7 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Version: "v2.0.1",
+	Version: "v2.0.2",
 	Use:     "ggen",
 	Short:   "Go project generator",
 	Long:    "A CLI for generating go project, it helps automate the process of creating a new Go project with a predefined directory structure, configuration files, and third party library/package.",
